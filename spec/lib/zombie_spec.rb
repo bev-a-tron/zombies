57f6a00f724878a542a4d515b2d9d68d7c1cc58a
Beverly Lau <beverly.a.lau@gmail.com>
2016-01-28 14:05:47 -0800

    Add specs for predicate matchers

--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -11,5 +11,24 @@ describe Zombie do
     zombie = Zombie.new
     expect(zombie.brains).to be < 1
   end
+end
 
+describe 'Predicate matchers' do
+  describe 'Fixnum#zero?' do
+    describe 0 do
+      it { should be_zero }
+    end
+    describe 7 do
+      it { should_not be_zero }
+    end
+  end
+
+  describe 'Array#empty?' do
+    describe [1, 2, 3] do
+      it { should_not be_empty}
+    end
+    describe [] do
+      it { should be_empty }
+    end
+  end
 end
